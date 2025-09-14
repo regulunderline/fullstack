fdf11348bc1ede76a1932a352442cdea3031e7b6
regul <regulunderline.com>
2025-09-14 17:55:34 +0300

    1.7

---
+++ part1/1.7/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const Average = ({good, neutral, bad}) => {
+  if (good+neutral+bad !== 0){
+    return (
+      <div>
+      <p>average {(good-bad)/(good+neutral+bad)}</p>
+      </div>
+    )}
+  return (
+    <div>
+      <p>average 0</p>
+    </div>
+  )
+}
+
+const Positive = ({good, neutral, bad}) => {
+  if (good+neutral+bad !== 0){
+    return (
+      <div>
+      <p>positive {100*good/(good+neutral+bad)} %</p>
+      </div>
+    )}
+  return (
+    <div>
+      <p>positive 0 %</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good+1)}>good</button>
+      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+      <button onClick={() => setBad(bad+1)}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good+neutral+bad}</p>
+      <Average good={good} neutral={neutral} bad={bad}/>
+      <Positive good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
