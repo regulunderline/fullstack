6f3e999f228b8c4451f039ab3bb0a39ce0581d8c
regul <regulunderline.com>
2025-09-15 17:40:34 +0300

    1.10

---
+++ part1/1.10/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+const StatisticLine = ({text, value}) => {
+  return(
+    <p>{text} {value}</p>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  if (good+neutral+bad>0){
+    return(
+      <div>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={good+neutral+bad}/>
+        <StatisticLine text="average" value={(good-bad)/(good+bad+neutral)}/>
+        <StatisticLine text="positive" value={[100*good/(good+neutral+bad), '%']}/>
+      </div>
+      )
+  }
+  else {
+    return(
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const Button = (good, neutral, bad) => {
+    setGood(good)
+    setBad(bad)
+    setNeutral(neutral)  
+}
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => Button(good+1, neutral, bad)}>good</button>
+      <button onClick={() => Button(good, neutral+1, bad)}>neutral</button>
+      <button onClick={() => Button(good, neutral, bad+1)}>bad</button>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
