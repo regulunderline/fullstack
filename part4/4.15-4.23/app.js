b01cf08e26ff4b046f29c3fa26fa0686ae221d25
regul <regulunderline@gmail.com>
2025-10-01 20:35:03 +0300

    4.15-4.23

---
+++ part4/4.15-4.23/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(express.json())
+
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+module.exports = app
